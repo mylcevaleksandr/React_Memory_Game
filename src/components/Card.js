cb3164f55e7e56ccbb24324530733cc2e7187848
mylcevaleksandr <myltsevaleksandr@gmail.com>
2025-03-26 02:06:04 +0300

    Add media query to make grid layout responsive. Add homepage to package.json and process.env.PUBLIC_URL for better resource loading

--- src/components/Card.js
+++ src/components/Card.js
@@ -12,10 +12,10 @@ export class Card extends React.Component {
             <div className={'card ' + (this.props.isOpen ? 'open' : 'closed')}
                  onClick={this.cardClickHandler.bind(this, this.props.item)}>
                 <div className="card-inner card-front">
-                    <img src={'/images/' + this.props.item.image} alt={this.props.item.name}/>
+                    <img src={process.env.PUBLIC_URL +'/images/' + this.props.item.image} alt={this.props.item.name}/>
                 </div>
                 <div className="card-inner card-back">
-                    <img src={'/images/question.svg'} alt="question mark"/>
+                    <img src={process.env.PUBLIC_URL +'/images/question.svg'} alt="question mark"/>
                 </div>
             </div>
         );
